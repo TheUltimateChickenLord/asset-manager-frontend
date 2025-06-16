226712ac43201d7d9f29202799cb610c80e26822
TheUltimateChickenLord <Alexander.B_2023_Lon@outlook.com>
2025-06-16 09:12:42 +0100

    feat(pages): Created error pages
    Created pages for 404 Not Found and 401 Unauthorized

---
+++ src/pages/Errors/NotFound.tsx
@@ -0,0 +1,11 @@
+import { Alert } from '@mui/material'
+
+const NotFound = () => {
+  return (
+    <Alert severity="error">
+      404: The page you are looking for cannot be found
+    </Alert>
+  )
+}
+
+export default NotFound
