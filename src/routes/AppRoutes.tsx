703d8c1e4748c0fbc7acd23a7eb2a9362785a305
TheUltimateChickenLord <Alexander.B_2023_Lon@outlook.com>
2025-06-16 09:57:12 +0100

    feat(routes): Added router
    Added router and placeholder for all routes

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,182 @@
+import { Routes, Route } from 'react-router-dom'
+import PrivateRoute from '../components/PrivateRoute'
+import NotFound from '../pages/Errors/NotFound'
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/login" element={<></>} />
+      <Route
+        path="/reset-password"
+        element={
+          <PrivateRoute>
+            <></>
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <></>
+          </PrivateRoute>
+        }
+      >
+        <Route
+          index
+          element={
+            <PrivateRoute>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="assets"
+          element={
+            <PrivateRoute roles={[['ReadAsset']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="assets/new"
+          element={
+            <PrivateRoute roles={[['CreateEditAsset']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="assets/:id/edit"
+          element={
+            <PrivateRoute roles={[['ReadAsset', 'CreateEditAsset']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="assets/:id/check-out"
+          element={
+            <PrivateRoute roles={[['CheckInOutAsset']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="assets/:id/request"
+          element={
+            <PrivateRoute roles={[['ReadAsset', 'RequestAsset']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="assets/:id"
+          element={
+            <PrivateRoute roles={[['ReadAsset']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="assets/:id/labels"
+          element={
+            <PrivateRoute roles={[['ReadAsset', 'CreateEditAsset']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="assets/:id/link"
+          element={
+            <PrivateRoute roles={[['ReadAsset', 'LinkAsset']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="users"
+          element={
+            <PrivateRoute roles={[['ReadUser']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="users/new"
+          element={
+            <PrivateRoute roles={[['CreateEditUser']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="users/:id"
+          element={
+            <PrivateRoute roles={[['ReadUser']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="users/:id/labels"
+          element={
+            <PrivateRoute roles={[['ReadUser', 'CreateEditUser']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="users/:id/roles"
+          element={
+            <PrivateRoute roles={[['ReadUser', 'CreateEditUser']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="requests"
+          element={
+            <PrivateRoute roles={[['CheckInOutAsset'], ['RequestAsset']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="requests/:id"
+          element={
+            <PrivateRoute roles={[['CheckInOutAsset'], ['RequestAsset']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="requests/:id/check-out"
+          element={
+            <PrivateRoute roles={[['CheckInOutAsset'], ['RequestAsset']]}>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="assignments"
+          element={
+            <PrivateRoute>
+              <></>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="assignments/:id"
+          element={
+            <PrivateRoute>
+              <></>
+            </PrivateRoute>
+          }
+        />
+
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  )
+}
